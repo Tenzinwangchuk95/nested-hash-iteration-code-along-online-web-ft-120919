bf75acf440e97b9eaa187a661e0aa31cc895a33b
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-13 19:25:57 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,8 @@ def remove_strawberry(contacts)
   contacts["Freddy Mercury"].each do |key, value|
     if key == :favorite_ice_cream_flavors
       value.each do |flavor|
-        flavor.delete_if { flavor == "strawberry" }
+        if { flavor == "strawberry"
+          flavor.delete}
       end
     end
   end
